3e21cf87ba86028b9d79cfe1e08810ece3c91a1d
Viet Hung Tran <trantechenterprise@gmail.com>
2017-04-03 15:32:39 -0400

    Add Index Of String And Char

--- stringutils/strinfo.c
+++ stringutils/strinfo.c
@@ -42,4 +42,68 @@ bool is_digit(string str) {
     return true;
 }
 
+//Returns the first index that has c character in the string str
+//Returns -1 if not found.
+int index_of_str_basic(string str, char c) {
+    assert(str!=NULL);
+    for (int i=0;str[i]!='\0';i++) {
+        if (str[i]==c) {return i;}
+    }
+    return -1;
+}
+
+//Returns the first index that has c character in the string str starting from index. 
+//Returns -1 if not found.
+int index_of_n_str(string str, char c, int index) {
+    assert(str!=NULL && len_str(str)>index && index>=0);
+    for (int i=index;str[i]!='\0';i++) {
+        if (str[i]==c) {return i;}
+    }
+    return -1;
+}
+
+//Returns the index within str of the first occurrence of the substring sub.
+//Return -1 if not found
+int index_of_str_sub(string str,string sub){
+    assert(str!=NULL && sub!=NULL && sub[0]!='\0');
+    int str_i=0,sub_i=0,fallback=-1;
+    while (str[str_i]!='\0' && sub[sub_i]!='\0') {
+        if (str[str_i]==sub[sub_i]) {
+            ++sub_i;
+            if (fallback==-1) {fallback=str_i;}
+        } 
+        else {
+            sub_i=0;
+            if (fallback!=-1) {
+                str_i=fallback;
+                fallback=-1;
+            }
+        }
+        ++str_i;
+    }
+    return sub[sub_i]=='\0' ? str_i-sub_i : -1; 
+}
+
+//Returns the index within str of the first occurrence of the substring subi, starting at specified index.
+//Return -1 if not found
+int index_of_n_str_sub(string str,string sub, int index) {
+    assert(str!=NULL && sub!=NULL && sub[0]!='\0' && len_str(str)>index && index>=0);
+    int str_i=index,sub_i=0,fallback=-1;
+    while (str[str_i]!='\0' && sub[sub_i]!='\0') {
+        if (str[str_i]==sub[sub_i]) {
+            ++sub_i;
+            if (fallback==-1) {fallback=str_i+1;}
+        } 
+        else {
+            sub_i=0;
+            if (fallback!=-1) {
+                str_i=fallback;
+                fallback=-1;
+            }
+        }
+        ++str_i;
+    }
+    return sub[sub_i]=='\0' ? str_i-sub_i : -1; 
+}
+
 
--- stringutils/strinfo.h
+++ stringutils/strinfo.h
@@ -20,6 +20,42 @@ bool ends_with_str(string str,string suffix);
 //Check if string only consists of digits
 bool is_digit(string str);
 
+//Returns the first index that has c character in the string str
+//Returns -1 if not found.
+//Can be used by calling index_of_str for short
+int index_of_str_basic(string str, char c);
+
+//Returns the first index that has c character in the string str starting from index. 
+//Returns -1 if not found.
+//Can be used by calling index_of_str for short
+int index_of_n_str(string str, char c, int index);
+
+//Returns the index within str of the first occurrence of the substring sub.
+//Return -1 if not found
+//Can be used by calling index_of_str for short
+int index_of_str_sub(string str,string sub);
+
+//Returns the index within str of the first occurrence of the substring subi, starting at specified index.
+//Return -1 if not found
+//Can be used by calling index_of_str for short
+int index_of_n_str_sub(string str,string sub, int index);
+
+#define INDEX_OF_3ARGS(_1,_2,_3) _Generic((_2),\
+        char:index_of_n_str,\
+        char*:index_of_n_str_sub)(_1,_2,_3)
+
+#define INDEX_OF_2ARGS(_1,_2) _Generic((_2),\
+        char:index_of_str_basic,\
+        char*:index_of_str_sub) (_1,_2) 
+
+#define _GET_INDEX_OF_OVERLOAD(_1,_2,_3,_N,...) _N
+
+#define index_of_str(...) _GET_INDEX_OF_OVERLOAD(\
+        __VA_ARGS__,\
+        INDEX_OF_3ARGS,\
+        INDEX_OF_2ARGS ) (__VA_ARGS__)
+
+#include "strutils.h"
 #include "strinfo.c"
 
 #endif
